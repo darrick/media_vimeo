462f431b875f6dd3c4eb6fecaa39ddcf980be46e
aaronwinborn <aaron@aaronwinborn.com>
2010-09-17 16:16:34 +0000

    by aaron: Initial commit of the Media: Vimeo project.

---
+++ themes/media-vimeo.tpl.php
@@ -0,0 +1,15 @@
+<?php
+// $Id$
+
+/**
+ * @file media_vimeo/themes/media-vimeo.tpl.php
+ *
+ * The template file for theme_media_vimeo.
+ *
+ * The output for this will be determined by either
+ * theme('media_vimeo_universal') or theme('media_vimeo_flash').
+ */
+?>
+<div id="media-vimeo-<?php print $id; ?>" class="media-vimeo">
+  <?php print $output; ?>
+</div>
